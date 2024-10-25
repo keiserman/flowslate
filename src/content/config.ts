2bb4920e66cc482da3f4af0f8fff5ca5ffa7b529
Kevin Eiserman <eisermanka@gmail.com>
2024-10-25 13:54:42 -0400

    Add copy button

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
-// Define the 'components' collection schema
 const components = defineCollection({
   schema: ({ image }) => z.object({
     title: z.string(),
     copy: z.string(),
-    image: image(), // Ensure this is correct to handle images
+    image: image(),
+    json: z.string(),
   }),
 });
 
 export const collections = {
-  components: components,  // Register the 'components' collection
+  components: components,
 };
